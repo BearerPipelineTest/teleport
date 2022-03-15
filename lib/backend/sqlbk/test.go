398b814af4db307af27e901379806819efadfa52
Jim Bishopp <jamesbishopp@gmail.com>
2022-03-15 15:09:20 -0700

    Add test coverage for purge

--- lib/backend/sqlbk/test.go
+++ lib/backend/sqlbk/test.go
@@ -19,7 +19,9 @@ package sqlbk
 import (
 	"context"
 	"testing"
+	"time"
 
+	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/lib/backend"
 	"github.com/gravitational/teleport/lib/backend/test"
 	"github.com/gravitational/trace"
@@ -38,22 +40,86 @@ func TestDriver(t *testing.T, driver Driver) {
 	fakeClock, ok := driver.Config().Clock.(clockwork.FakeClock)
 	require.True(t, ok, "expected %v driver to configure a FakeClock", driver.BackendName())
 
-	newBackend := func(options ...test.ConstructionOption) (backend.Backend, clockwork.FakeClock, error) {
-		opts, err := test.ApplyOptions(options)
-		if err != nil {
-			return nil, nil, trace.Wrap(err)
-		}
+	t.Run("Backend Compliance Suite", func(t *testing.T) {
+		newBackend := func(options ...test.ConstructionOption) (backend.Backend, clockwork.FakeClock, error) {
+			opts, err := test.ApplyOptions(options)
+			if err != nil {
+				return nil, nil, trace.Wrap(err)
+			}
 
-		if opts.MirrorMode {
-			return nil, nil, test.ErrMirrorNotSupported
-		}
+			if opts.MirrorMode {
+				return nil, nil, test.ErrMirrorNotSupported
+			}
 
-		bk := &testBackend{Backend: bk}
-		bk.buf = backend.NewCircularBuffer(backend.BufferCapacity(bk.BufferSize))
-		bk.buf.SetInit()
-		return bk, fakeClock, nil
-	}
-	test.RunBackendComplianceSuite(t, newBackend)
+			bk := &testBackend{Backend: bk}
+			bk.buf = backend.NewCircularBuffer(backend.BufferCapacity(bk.BufferSize))
+			bk.buf.SetInit()
+			return bk, fakeClock, nil
+		}
+		test.RunBackendComplianceSuite(t, newBackend)
+	})
+
+	t.Run("Purge", func(t *testing.T) {
+		// purge calls 3 Tx methods:
+		// - DeleteExpiredLeases
+		// - DeleteEvents(before)
+		// - DeleteItems(before)
+		//
+		// Scenario:
+		// - Create 4 items (a, b, c, d)
+		//   - a/c are active
+		//   - b/d have expired
+		// - Call purge with d-1 event ID
+		// - Confirm:
+		//   - b item removed (no event or lease)
+		//   - b/d leases removed (expired)
+		//   - a/b/c events removed (fromEventID < d-1)
+
+		// Create items
+		v := []byte("value")
+		a := backend.Item{Value: v, Key: []byte("/purgetest/a")}                                             // active
+		b := backend.Item{Value: v, Key: []byte("/purgetest/b"), Expires: fakeClock.Now().Add(-time.Second)} // expired
+		c := backend.Item{Value: v, Key: []byte("/purgetest/c"), Expires: fakeClock.Now().Add(time.Second)}  // active
+		d := backend.Item{Value: v, Key: []byte("/purgetest/d"), Expires: fakeClock.Now().Add(-time.Second)} // expired with event
+
+		tx := bk.db.Begin(context.Background())
+		a.ID = tx.InsertItem(a)
+		b.ID = tx.InsertItem(b)
+		c.ID = tx.InsertItem(c)
+		d.ID = tx.InsertItem(d)
+		tx.UpsertLease(a)
+		tx.UpsertLease(b)
+		tx.UpsertLease(c)
+		tx.UpsertLease(d)
+		tx.InsertEvent(types.OpPut, a)
+		tx.InsertEvent(types.OpPut, b)
+		tx.InsertEvent(types.OpPut, c)
+		tx.InsertEvent(types.OpPut, d)
+		fromEventID := tx.GetLastEventID()
+		require.NoError(t, tx.Commit())
+
+		// Purge
+		require.NoError(t, bk.purge(fromEventID-1))
+
+		// Validate results.
+		tx = bk.db.ReadOnly(context.Background())
+		t.Cleanup(func() { tx.Commit() })
+
+		lastEventID, events := tx.GetEvents(0, 10)
+		require.Equal(t, lastEventID, fromEventID)
+		require.Equal(t, 1, len(events))
+		require.Equal(t, d.Key, events[0].Item.Key)
+
+		require.True(t, tx.LeaseExists(a.Key))
+		require.False(t, tx.LeaseExists(b.Key))
+		require.True(t, tx.LeaseExists(c.Key))
+		require.False(t, tx.LeaseExists(d.Key))
+
+		items := tx.GetItemRange(a.Key, d.Key, 10)
+		require.Equal(t, 2, len(items))
+		require.Equal(t, items[0].Key, a.Key)
+		require.Equal(t, items[1].Key, c.Key)
+	})
 }
 
 // testBackend wraps Backend overriding Close.
